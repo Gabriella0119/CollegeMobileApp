83f7cce4effbc709815b5f3d30253b94111afafa
Gabriella0119 <86381046+Gabriella0119@users.noreply.github.com>
2021-06-23 15:43:48 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+} 
+
+
